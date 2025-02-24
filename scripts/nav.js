5b0490e2889bade049962a72869409277ed6f675
Nina Sekulic <nina.sekulic@outlook.com>
2025-02-24 12:03:09 -0500

    Updated mobile nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,10 @@
 //mobile nav bar open and close
 function showNav() {
     var links = document.getElementById("links");
-    if (links.style.display == "block") {
+    if (links.style.display == "flex") {
         links.style.display = "none";
     } else {
-        links.style.display = "block";
+        links.style.display = "flex";
     }
 }
+
